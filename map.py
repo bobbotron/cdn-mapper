dc0f4a8d911fac354655d71ab13cb586f0adc225
Robert Young <bobbotron@gmail.com>
2023-10-07 16:30:02 -0400

    Fixed requirements.txt, project now runs.

--- map.py
+++ map.py
@@ -15,13 +15,14 @@ canada = geopandas.read_file("./gfsa000b11a_e.shp")
 ontario = canada[canada['PRUID'] == '35']
 
 # Sample data to plot
-df=pd.DataFrame({'PCODE': ['P0V','P0L','P0T','P0Y', 'P0G', 'P2N'], 'A':[6,3,5,2,2,4] })
+df=pd.read_csv('survey-data.csv');
+#df=DataFrame({'PCODE': ['P0V','P0L','P0T','P0Y', 'P0G', 'K0G'], 'A':[6,3,5,2,2,42] })
 
 # Join ontario dataset with sample data
-new_df=ontario.join(df.set_index('PCODE'), on='CFSAUID')
+new_df=ontario.join(df.set_index('PCODE'), on='CFSAUID').fillna(0)
 
 
-new_df.plot_bokeh(simplify_shapes=20000,
+new_df.plot_bokeh(#simplify_shapes=20000,
                   category="A",
                   colormap="Spectral", 
                   hovertool_columns=["CFSAUID","A"])
