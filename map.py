c0f0e91c461fb4c0dfbf298f0d77c653668631f5
Robert Young <bobbotron@gmail.com>
2023-10-07 17:52:23 -0400

    Code now works, added some data for reference.

--- map.py
+++ map.py
@@ -16,13 +16,23 @@ ontario = canada[canada['PRUID'] == '35']
 
 # Sample data to plot
 df=pd.read_csv('survey-data.csv');
-#df=DataFrame({'PCODE': ['P0V','P0L','P0T','P0Y', 'P0G', 'K0G'], 'A':[6,3,5,2,2,42] })
+
+# Load pop data per FSA and just use the population data
+fsa_pop_df=pd.read_csv('9810001901_databaseLoadingData.csv')
+fsa_pop_df=fsa_pop_df[fsa_pop_df['Population and dwelling counts (3)'].str.contains('Population, 2021')]
+fsa_pop_df=fsa_pop_df[['GEO','VALUE']].copy().rename(columns={"GEO":"GEO", "VALUE": "POP_COUNT"})
+
+# Join our sample data to FSA geo code
+df=df.join(fsa_pop_df.set_index('GEO'), on='PCODE');
+
+# Normalize per 100,000 people
+df["B"]=100000*df["A"]/df["POP_COUNT"]
 
 # Join ontario dataset with sample data
-new_df=ontario.join(df.set_index('PCODE'), on='CFSAUID').fillna(0)
+new_df=ontario.join(df.set_index('PCODE'), on='CFSAUID').dropna()
+#fillna(0)
 
-
-new_df.plot_bokeh(#simplify_shapes=20000,
-                  category="A",
+new_df.plot_bokeh(category="B",
+                  title="Responses per 100,000 pop",
                   colormap="Spectral", 
-                  hovertool_columns=["CFSAUID","A"])
+                  hovertool_columns=["CFSAUID","A","B", "POP_COUNT"])
