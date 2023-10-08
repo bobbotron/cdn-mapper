6d2fa38db518d3e61d75f370329cbd77e69f931d
Robert Young <bobbotron@gmail.com>
2023-10-08 11:17:11 -0400

    Update color mapping

--- map.py
+++ map.py
@@ -2,6 +2,8 @@ import geopandas
 import pandas as pd
 import pandas_bokeh
 import matplotlib.pyplot as plt
+import colorsys
+
 pandas_bokeh.output_notebook()
 
 # Download this from 
@@ -32,7 +34,14 @@ df["B"]=100000*df["A"]/df["POP_COUNT"]
 new_df=ontario.join(df.set_index('PCODE'), on='CFSAUID').dropna()
 #fillna(0)
 
+num_colors = 10
+colors = [colorsys.hsv_to_rgb(213/360, 0.398, 1 - x*0.9/num_colors) for x in range(num_colors)]
+rgb = map(lambda x : '#{:02x}{:02x}{:02x}'.format(int(x[0]*255),int(x[1]*255),int(x[2]*255)), colors)
+
 new_df.plot_bokeh(category="B",
                   title="Responses per 100,000 pop",
-                  colormap="Spectral", 
+                  colormap = list(rgb),
+                  figsize=(900, 600),
+                  # colormap=["#6B8BB3", "#4D6480", "#2E3C4D", "#0F141A", "#080A0D"],
+                  # colormap="Blues", #  colormap=["#009933", "#ff3399"],
                   hovertool_columns=["CFSAUID","A","B", "POP_COUNT"])
